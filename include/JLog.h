cfdc1e8347d4ed9f3ef4bc0101e8e7bdec368412
dingjingMaster <dingjing@live.cn>
2018-02-07 16:31:13 +0800

    加入log

---
+++ include/JLog.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+> FileName: JLog.h
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Wed 22 Nov 2017 06:59:34 PM DST
+ ************************************************************************/
+
+#ifndef _JLOG_H
+#define _JLOG_H
+#include "zlog.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FATAL(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_FATAL, __VA_ARGS__)
+#define ERROR(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_ERROR, __VA_ARGS__)
+#define WARN(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_WARN, __VA_ARGS__)
+#define NOTICE(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_NOTICE, __VA_ARGS__)
+#define INFO(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_INFO, __VA_ARGS__)
+#define DEBUG(...) \
+	dzlog(__FILE__, sizeof(__FILE__)-1, __func__, sizeof(__func__)-1, __LINE__, \
+	ZLOG_LEVEL_DEBUG, __VA_ARGS__)
+
+void log_init(char* confPath);
+
+void log_destory(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/JLog.cc
@@ -0,0 +1,35 @@
+/*************************************************************************
+> FileName: JLog.c
+> Author  : DingJing
+> Mail    : dingjing@live.cn
+> Created Time: Wed 22 Nov 2017 06:59:38 PM DST
+ ************************************************************************/
+
+#include "JLog.h"
+#include <stdio.h>
+
+void log_init(char* confPath) {
+    
+    if(NULL == confPath) {
+        
+        ERROR("log_init error: %s", confPath);
+        return ;
+    }
+    
+    int                     rc = 0;
+
+    rc = dzlog_init(confPath, "");
+    if(rc) {
+        
+        ERROR("dzlog_init error ret: %d", rc);
+        
+        return ;
+    }
+}
+
+void log_destory(void){
+    
+    zlog_fini();
+}
+
+
